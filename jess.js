edb0178532a0a6f81f9903fb9a5ee2eb0b282c9e
CensedPie <alex.savu@hotmail.com>
2018-02-18 01:00:50 +0200

    Create folders and files on join

--- jess.js
+++ jess.js
@@ -14,6 +14,11 @@ bot.on("ready", () => {
 bot.on("guildCreate", guild => {
     // This event triggers when the bot joins a server.
     console.log('New guild joined: ' + guild.name +' (id: ' + guild.id + '). This guild has ' + guild.memberCount + ' members!');
+    
+    //Create folder and files.
+    for(chan of guild.channels) {
+        logger.myWrite("", chan.name, "@" + guild.id);
+    }
 });
 
 bot.on("guildDelete", guild => {
@@ -43,18 +48,28 @@ bot.on("message", async message => {
     const command = args.shift().toLowerCase();
 
     if(command === "mute") {
+
+        //Boolean for if someone was found to mute.
         var muted = false;
 
+        //Find Muted role.
         var role = message.guild.roles.find("name", "Muted");
 
-        for(user of client.users) {
-            var member = message.mentions.members.first();
-            member.setRoles([role]).catch(console.error);
-            muted = true;
+        //Check if role exists so bot can add to muted role.
+        if(role === null) {
+            message.channel.send("Muted role doesn't exist. Please create a role named Muted where the user does not have permissions to write messages.");
+            return;
         }
 
+        //Get mentioned user and set their role.
+        var member = message.mentions.members.first();
+        member.setRoles([role]).catch(console.error);
+        muted = true;
+
+        //If no one was found then send message about it.
         if(!muted) {
             message.channel.send("Can't find that person to mute.");
+            return;
         }
     }
 });
