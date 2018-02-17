3f8ff34c8159633900a990ac7c5ca6a9070ae1c3
CensedPie <alex.savu@hotmail.com>
2018-02-17 22:35:18 +0200

    Updated auth.json and jess.js
    added .gitignore for auth.json to allow testing with token.

--- jess.js
+++ jess.js
@@ -26,6 +26,12 @@ bot.on("message", async message => {
 
     //If the message doesn't have the identifier .jess don't process it.
     if(message.content.indexOf(auth.prefix) !== 0) return;
+
+    //Split and trim the message content into an array of args = ["arg1", "arg2", "arg3"]
+    //Ignores prefix because of slice command
+    const args = message.content.slice(auth.prefix.length).trim().split(/ +/g);
+    //Args to lower case for processing in case people write ARG1 instead of arg1.
+    const command = args.shift().toLowerCase();
 });
 
 //Log the bot in.
