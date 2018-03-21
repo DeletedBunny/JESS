ee65e26d341c7e1a00ad818b52108d57e7208d97
BrianSQL <brian.kropilak@gmail.com>
2018-03-21 13:53:03 -0400

    OOP Commands, Awaits/Async, Avatar function, standarized class format for any new function
    Reworked programming for better runtime efficiency. Added new Avatar Command. Standarized a format for any new command added.

---
+++ commands/avatar.js
@@ -0,0 +1,36 @@
+const Discord = require("discord.js");
+
+class avatar {
+ 
+    static async run(jbot, message, args) {
+        let loadMessage = await message.channel.send("Loading "+message.author.username+"'s Avatar")
+
+        await message.channel.send({files:[{
+            attachment: message.author.displayAvatarURL,
+            name: "avatar.png"
+        }]});
+
+        await loadMessage.delete();
+    }
+
+    static async runPost(jbot, message, args) {
+
+        if (typeof (message.mentions.members.first()) === "undefined") {
+            this.run(jbot, message, args);
+            return;
+        }
+        
+        let member = message.mentions.members.first();
+        let loadMessage = await message.channel.send("Loading "+member.user.username+"'s Avatar")
+
+        await message.channel.send({files:[{
+            attachment: member.user.displayAvatarURL,
+            name: "avatar.png"
+        }]});
+        await loadMessage.delete();
+}
+
+}
+
+module.exports = avatar;
+module.exports.name = avatar;
--- commands/userinfo.js
+++ commands/userinfo.js
@@ -3,28 +3,29 @@ const Discord = require("discord.js");
 class userinfo {
 
     static async run(jbot, message, args) {
+        let loadMessage = await message.channel.send("Loading "+message.author.username+"'s User Information")
         let embed = new Discord.RichEmbed()
             .setColor("#9B59B6")
             .addField("Full Username", `${message.author.username}#${message.author.discriminator}`)
             .addField("ID", message.author.id)
             .addField("Created At", message.author.createdAt);
         // Send user-specific information to the channel.
-        message.channel.send(embed);
+        await message.channel.send(embed);
+        await loadMessage.delete();
     }
 
     static async runPost(jbot, message, args) {
 
-        //console.log("post command is "+postcom);    
-        //if (message!=="") console.log("test "+ message + args);
         // Get mentioned user for user info checking. 
         //var member = message.mentions.members.first();
-        if (typeof (message.mentions.members.first()) !== "undefined") {
-            var member = message.mentions.members.first();
-        }
-        else {
+        if (typeof (message.mentions.members.first()) === "undefined") {
             this.run(jbot, message, args);
             return;
         }
+        
+        let member = message.mentions.members.first();
+        let loadMessage = await message.channel.send("Loading "+member.user.username+"'s Avatar")
+
         // Set embed properties and fields for user info output.
         let embed = new Discord.RichEmbed()
             .setColor("#9B59B6")
@@ -32,7 +33,8 @@ class userinfo {
             .addField("ID", member.user.id)
             .addField("Created At", member.user.createdAt);
         // Send user-specific information to the channel.
-        message.channel.send(embed);
+        await message.channel.send(embed);
+        await loadMessage.delete();
     }
 
 }
